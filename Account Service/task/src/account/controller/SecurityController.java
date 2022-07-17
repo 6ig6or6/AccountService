2c234ab9ff335104aa695ca479e355700e3b3dd1
Igor <gyupper1@gmail.com>
2022-07-17 22:05:43 +0300

    Initial commit

---
+++ Account Service/task/src/account/controller/SecurityController.java
@@ -0,0 +1,27 @@
+package account.controller;
+
+import account.model.action.Event;
+
+import account.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/security")
+public class SecurityController {
+
+    private final EventService eventService;
+
+    @Autowired
+    public SecurityController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events")
+    public List<Event> showAllEvents() {
+        return eventService.showAllLogs();
+    }
+}
